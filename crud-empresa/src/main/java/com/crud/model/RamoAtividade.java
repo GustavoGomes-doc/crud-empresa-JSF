066d73f3b02135198aadf0b65b2792f605f8dba4
GustavoGomes <ggaprado@gmail.com>
2026-04-30 22:50:23 -0300

    criação dos objetos empresa, Ramo Atividade e TipoEmpresa e mapeando instacias, primeiro commit projeto CRUD empresa

---
+++ crud-empresa/src/main/java/com/crud/model/RamoAtividade.java
@@ -0,0 +1,64 @@
+package com.crud.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity 
+@Table(name = "ramo_atividade")
+public class RamoAtividade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String descrição;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescrição() {
+		return descrição;
+	}
+
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(descrição, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RamoAtividade other = (RamoAtividade) obj;
+		return Objects.equals(descrição, other.descrição) && Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "RamoAtividade [id=" + id + "]";
+	}
+
+	
+	
+}
---
+++ crud-empresa/src/main/java/com/crud/model/TipoEmpresa.java
@@ -0,0 +1,22 @@
+package com.crud.model;
+
+public enum TipoEmpresa {
+	MEI("Microempreendedor Individual"),
+	EIRELI("Empresa Individual de Responsabilidade Limitada"),
+	LTDA("Sociedade Limitada"),
+	SA("Sociedade Anônima");
+	
+	private String descricao;
+	
+	TipoEmpresa(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescrição(String descricao) {
+		this.descricao = descricao;
+	}
+}
